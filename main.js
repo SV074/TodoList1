912c02c98234e140897924f291cb3db47d5477be
SV074 <gabidulin174@gmail.com>
2022-11-06 17:51:14 +0300

    Четвертый

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ let list = document.querySelector('.list');
 let toDoList = [];
 
 
-    if(localStorage.getItem('toDo')) {
-        
-        toDoList = JSON.parse(localStorage.getItem('toDo')); 
-        displayMessages();
-    }
+if (localStorage.getItem('toDo')) {
+
+    toDoList = JSON.parse(localStorage.getItem('toDo'));
+    displayMessages();
+}
 
 
 btnAddTask.addEventListener('click', function () {
@@ -19,12 +19,12 @@ btnAddTask.addEventListener('click', function () {
         checked: false,
         important: false,
     };
-    
+
     toDoList.push(newToDo);
     displayMessages();
-    
-    localStorage.setItem('toDo',JSON.stringify(toDoList));
-    
+
+    localStorage.setItem('toDo', JSON.stringify(toDoList));
+
 
 
 
@@ -32,15 +32,50 @@ btnAddTask.addEventListener('click', function () {
 
 function displayMessages() {
     let displayMessage = '';
+    if(toDoList.length === 0) list.innerHTML = '';
     toDoList.forEach(function (item, i) {
 
         displayMessage += `
     <li>
         <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-        <label for='item_${i}'>${item.todo}</label>
+        <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
+        <span class='close'>X</sapan>
     </li>
     `;
         list.innerHTML = displayMessage;
     });
 
 }
+
+
+list.addEventListener('change', function(event) {
+    let idInput = event.target.getAttribute('id');
+    let forLabel = document.querySelector('[for='+ idInput +']');
+    let valueLabel = forLabel.innerHTML;
+    
+    toDoList.forEach(function(item) {
+        if(item.todo === valueLabel) {
+            item.checked = !item.checked;
+            localStorage.setItem('toDo', JSON.stringify(toDoList));
+        }
+    });
+
+
+});
+
+list.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+    toDoList.forEach(function(item, i) {
+        if(item.todo === event.target.innerHTML) {
+            if(event.delKey|| event.ctrlKey) {
+                toDoList.splice(i, 1);
+            } else {
+                item.important = !item.important;
+            }
+            
+            displayMessages();
+            localStorage.setItem('toDo', JSON.stringify(toDoList));
+        }
+    })
+});
+
