b8768f4febb5abf727c457ae5119ed841935d656
SV074 <gabidulin174@gmail.com>
2022-11-07 16:16:31 +0300

    Пятый

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let inputTask = document.querySelector('.input-task');
 let btnAddTask = document.querySelector('.add-task');
 let list = document.querySelector('.list');
-
+let btnClose = document.querySelector('.close');
 
 let toDoList = [];
 
+// Условие при котором, при обновлении страницы задачи не исчезают
 
 if (localStorage.getItem('toDo')) {
 
@@ -12,23 +13,29 @@ if (localStorage.getItem('toDo')) {
     displayMessages();
 }
 
+// Добавлении задачи
 
 btnAddTask.addEventListener('click', function () {
     let newToDo = {
+        id: Date.now(),
         todo: inputTask.value,
         checked: false,
         important: false,
     };
+    if (inputTask.value !== '') {
+        toDoList.push(newToDo);
+        displayMessages();
 
-    toDoList.push(newToDo);
-    displayMessages();
-
-    localStorage.setItem('toDo', JSON.stringify(toDoList));
+        localStorage.setItem('toDo', JSON.stringify(toDoList));
+    }
+    
 
 
 
 
 })
+
+// Функция отображения задачи
 
 function displayMessages() {
     let displayMessage = '';
@@ -36,10 +43,10 @@ function displayMessages() {
     toDoList.forEach(function (item, i) {
 
         displayMessage += `
-    <li>
+    <li class='li'>
         <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
         <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
-        <span class='close'>X</sapan>
+        <span class='close' data-delete='${item.id}'>X</sapan>
     </li>
     `;
         list.innerHTML = displayMessage;
@@ -47,6 +54,7 @@ function displayMessages() {
 
 }
 
+// Отмечаем завершенную задачу
 
 list.addEventListener('change', function(event) {
     let idInput = event.target.getAttribute('id');
@@ -63,19 +71,38 @@ list.addEventListener('change', function(event) {
 
 });
 
-list.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-    toDoList.forEach(function(item, i) {
-        if(item.todo === event.target.innerHTML) {
-            if(event.delKey|| event.ctrlKey) {
-                toDoList.splice(i, 1);
-            } else {
-                item.important = !item.important;
-            }
+// list.addEventListener('contextmenu', function(event) {
+//     event.preventDefault();
+//     toDoList.forEach(function(item, i) {
+//         if(item.todo === event.target.innerHTML) {
+//             if(event.delKey|| event.ctrlKey) {
+//                 toDoList.splice(i, 1);
+//             } else {
+//                 item.important = !item.important;
+//             }
             
-            displayMessages();
-            localStorage.setItem('toDo', JSON.stringify(toDoList));
-        }
-    })
-});
+//             displayMessages();
+//             localStorage.setItem('toDo', JSON.stringify(toDoList));
+//         }
+//     })
+// });
 
+// btnClose.addEventListener('click', function(event) {
+    
+// });
+
+list.addEventListener('click', function(event) {
+
+    // Проверяем был ли клик по кнопке удалить задачу
+
+    if(event.target.classList.contains('close')) {
+        let productId = event.target.dataset.delete;
+        console.log(productId);
+        let findIndex = toDoList.findIndex((item) => {
+            return productId === item.id;
+        })
+        toDoList.splice(findIndex,1);
+        displayMessages();
+        localStorage.setItem('toDo',JSON.stringify(toDoList));
+    }
+})
